d49482a54ec75237355c0d9ddd90cd1914d0c944
Dhir DeRay <dhirderay@Dhirs-MacBook-Pro.local>
2024-09-09 16:37:28 -0400

    commit 2

---
+++ users/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import UserRegisterForm
+from django.contrib import messages
+
+
+def home(request):
+    return render(request, 'users/home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, f'Your account has been created!')
+            return redirect('home')  # Redirect to home or wherever you'd like
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+# Create your views here.
